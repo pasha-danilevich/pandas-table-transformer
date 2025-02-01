7521892eea8025490d61fbecdae98bef0c1beaf7
pasha-danilevich <pasha-danilevich@outlook.com>
2025-02-01 20:47:02 +0300

    LicensesOnlyNormalizer

--- src/data_normalizer.py
+++ src/data_normalizer.py
@@ -6,11 +6,11 @@ class BaseDataNormalizer:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
 
-    def _get_parsed_services_lines(self, row: str) -> List[Tuple[str, str, str]]:
+    def _get_parsed_services_lines(self, row: str, split_symbol: str) -> List[Tuple[str, str, str]]:
         """
         Парсит строки услуг и возвращает список кортежей с исходной строкой, номером и описанием.
         """
-        service_lines = row.split("\n")
+        service_lines = row.split(split_symbol)
         result: List[Tuple[str, str, str]] = []
 
         for line in service_lines:
@@ -28,6 +28,7 @@ class BaseDataNormalizer:
     def __parse_line(self, line: str) -> Tuple[str, str]:
         """Разделяет строку на номер и описание."""
         if "." in line:
+            line = line.strip()
             number, description = line.split(" ", 1)
             number = number.strip()
             description = description.strip()
@@ -36,30 +37,6 @@ class BaseDataNormalizer:
             description = line.strip()
 
         return number, description
-
-    def _make_new_record(
-        self,
-        count: int,
-        row: pd.Series,
-        line: str,
-        number: str,
-        description: str
-    ) -> pd.DataFrame:
-        """Создает новую запись в виде DataFrame."""
-        new_record = pd.DataFrame({
-            "Номер": [count],
-            "Регистрационный номер": [row["Регистрационный номер"]],
-            "Дата поступления": [row["Дата поступления"]],
-            "Вид деятельности": [row["Вид деятельности"]],
-            "Уведомитель": [row["Уведомитель"]],
-            "ИНН": [row["ИНН"]],
-            "Адрес объекта осуществления": [row["Адрес объекта осуществления"]],
-            "ФИАС": [row["ФИАС"]],
-            "Работы и услуги": [line.strip()],
-            "__": [number],
-            "Деятельность": [description],
-        }, dtype="object")
-        return new_record
 
 
 class NotificationOnlyNormalizer(BaseDataNormalizer):
@@ -79,7 +56,7 @@ class NotificationOnlyNormalizer(BaseDataNormalizer):
             if pd.isna(row["Работы и услуги"]):
                 continue  # Пропускаем пустые строки
 
-            parsed_services_lines = self._get_parsed_services_lines(row["Работы и услуги"])
+            parsed_services_lines = self._get_parsed_services_lines(row["Работы и услуги"], split_symbol="\n")
 
             if not pd.isna(row["Регистрационный номер"]):
                 # Если 'Регистрационный номер' не пустой, записываем в запасную строку
@@ -116,20 +93,96 @@ class NotificationOnlyNormalizer(BaseDataNormalizer):
                         result = pd.concat([result, new_record], ignore_index=True)
                         count += 1
 
-            # if count == 100:
+            # if count >= 10:
             #     break
 
         print("Done.")
         return result
 
+    def _make_new_record(
+        self,
+        count: int,
+        row: pd.Series,
+        line: str,
+        number: str,
+        description: str
+    ) -> pd.DataFrame:
+        """Создает новую запись в виде DataFrame."""
+        new_record = pd.DataFrame({
+            "Номер": [count],
+            "Регистрационный номер": [row["Регистрационный номер"]],
+            "Дата поступления": [row["Дата поступления"]],
+            "Вид деятельности": [row["Вид деятельности"]],
+            "Уведомитель": [row["Уведомитель"]],
+            "ИНН": [row["ИНН"]],
+            "Адрес объекта осуществления": [row["Адрес объекта осуществления"]],
+            "ФИАС": [row["ФИАС"]],
+            "Работы и услуги": [line.strip()],
+            "__": [number],
+            "Деятельность": [description],
+        }, dtype="object")
+        return new_record
 
 class LicensesOnlyNormalizer(BaseDataNormalizer):
-    def __init__(self, df_list: list[pd.DataFrame]) -> None:
-        self.df_list = df_list
+
         
     def normalize(self) -> pd.DataFrame:
         """Нормализует данные и возвращает DataFrame."""
-        for df in self.df_list:
-            print(df.columns)
+        print("In process...")
+
+        # Создаем пустой DataFrame для результата
+        result = pd.DataFrame(columns=list(self.df.columns)) 
+        
+        
+        for _, row in self.df.iterrows():
+            # Проверяем, является ли вся строка пустой (все значения NaN)
+            if pd.isna(row["Работы/услуги"]):
+                continue  # Пропускаем пустые строки
             
-        return self.df_list[0]
+            parsed_services_lines = self._get_parsed_services_lines(row["Работы/услуги"], split_symbol=';')
+            
+            for line_tuple in parsed_services_lines:
+                line, number, description = line_tuple
+
+                new_record = self._make_new_record(
+                    row=row,
+                    line=line,
+                    number=number,
+                    description=description,
+                )
+
+                result = pd.concat([result, new_record], ignore_index=True)
+                
+                
+
+        return result
+
+    def _make_new_record(
+        self,
+        row: pd.Series,
+        line: str,
+        number: str,
+        description: str
+    ) -> pd.DataFrame:
+        """Создает новую запись в виде DataFrame."""
+        new_record = pd.DataFrame({
+            "Наименование организации": [row["Наименование организации"]],
+            "Полное наименование организации": [row["Полное наименование организации"]],
+            "ИНН": [row["ИНН"]],
+            "ОГРН": [row["ОГРН"]],
+            "Регион организации": [row["Регион организации"]],
+            "Юридический адрес": [row["Юридический адрес"]],
+            "Регион объекта": [row["Регион объекта"]],
+            "Адрес объекта": [row["Адрес объекта"]],
+            "Тип населенного пункта": [row["Тип населенного пункта"]],
+            "Код ФИАС": [row["Код ФИАС"]],
+            "Тип объекта": [row["Тип объекта"]],
+            "№": [row["№"]],
+            "Дата": [row["Дата"]],
+            "Статус": [row["Статус"]],
+            
+            "Работы/услуги": [line.strip()],
+            "__": [number],
+            "Деятельность": [description],
+        }, dtype="object")
+        return new_record
--- src/main.py
+++ src/main.py
@@ -29,15 +29,15 @@ def normalize_list(input_files: list[FileConfig], output_path: str):
     
     # Чтение данных из Excel-файла
     df_list: list[pd.DataFrame] = []
-    for df in input_files:
+    for file in input_files:
         df = pd.read_excel(
-            io=input_file.get_relative_file_path(),
-            skiprows=input_file.skiprows
+            io=file.get_relative_file_path(),
+            skiprows=file.skiprows
         )
         df_list.append(df)
 
-    
-    normalizer_obj = LicensesOnlyNormalizer(df_list)  # Создаем экземпляр нормализатора
+    df = pd.concat(df_list, axis=0)
+    normalizer_obj = LicensesOnlyNormalizer(df)  # Создаем экземпляр нормализатора
     normalized_df = normalizer_obj.normalize()
 
     # Сохранение нормализованных данных в Excel
@@ -57,14 +57,34 @@ if __name__ == '__main__':
         normalize(input_file, output_path)
         
     
-    # input_files = [FileConfig('Подведы_ГБУ вариант 2.xlsx', 12),
-    #                FileConfig('Подведы МЗ РК санатории.xlsx', 13),
-    #                FileConfig('Подведы МЗ РК_ГАУ.xlsx', 13),
-    #             ]
+    input_files = [FileConfig('Подведы_ГБУ вариант 2.xlsx', 12),
+                   FileConfig('Подведы МЗ РК_ГАУ.xlsx', 13),
+                   FileConfig('Подведы МЗ РК санатории.xlsx', 13),
+                   FileConfig('Семашко расширенный.xlsx', 12),]
     
-    # output_file_name = "normalized_license"
+    
+    
+    output_file_name = "normalized_license"
 
-    # # Проверка прав на запись в файл
-    # output_path = f"{BASE_NORMALIZED_DATA_DIR}/{output_file_name}.xlsx"
-    # if FileConfig.check_write_permission(output_path):
-    #     normalize_list(input_files, output_path)
+    # Проверка прав на запись в файл
+    output_path = f"{BASE_NORMALIZED_DATA_DIR}/{output_file_name}.xlsx"
+    if FileConfig.check_write_permission(output_path):
+        normalize_list(input_files, output_path)
+    
+
+
+    # df_notification = pd.read_excel(
+    #     io=f'{BASE_NORMALIZED_DATA_DIR}/normalized_notifications.xlsx',
+    #     skiprows=0
+    # )
+    # df_license = pd.read_excel(
+    #     io=f'{BASE_NORMALIZED_DATA_DIR}/normalized_license.xlsx',
+    #     skiprows=0
+    # )
+
+    # # Объединение DataFrame по ИНН
+    # result = pd.merge(df_notification, df_license, on='ИНН', how='outer')
+    
+    # result.to_excel(f"{BASE_NORMALIZED_DATA_DIR}/notification_license.xlsx", index=False) ValueError: This sheet is too large! Your sheet size is: 8848380, 27 Max sheet size is: 1048576, 16384
+    # print(f"Данные успешно сохранены")
+    
